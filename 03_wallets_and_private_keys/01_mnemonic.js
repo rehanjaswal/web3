6265cbfa26de5e0d90ba583f3aa02d216b4db43a
rehanjaswal <rehanxjaswal@gmail.com>
2026-02-25 23:59:10 +0530

    nmemonicToSeed

--- 03_wallets_and_private_keys/01_mnemonic.js
+++ 03_wallets_and_private_keys/01_mnemonic.js
@@ -1,12 +1,12 @@
 import { generateMnemonic } from 'bip39';
 
 
-const mnemonic = generateMnemonic(256);    // entropy = randomness of password (basically strength)
+const mnemonic = generateMnemonic(256);    // 256 will generate a 12 word mnemonic; entropy = randomness of password (basically strength)
 console.log(mnemonic)
 
 // 2048 word dictionary, 2 to the power 11 is 2048, so each word contains 11 bit data.
 // more the entropy, more secure and strong is the passcode
 // checksum = entropy / 2
 // then add bits, add = entropy + checksum
-// words = (entropy + checksum) / 11    -> divide by 11 cuz each word contains 11 bits, so total bits divided by no. of bits in one word will give us total no of words
+// words = (entropy + checksum) / 11 -> divide by 11 cuz each word contains 11 bits, so total bits divided by no. of bits in one word will give us total no of words
 
